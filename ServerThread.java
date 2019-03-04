e00c77a0a8c8d1b93d30e4ea5c2699401e313ba5
nathanking1998 <43959646+nathanking1998@users.noreply.github.com>
2019-03-04 14:02:51 +0000

    Initial Upload of working Client Server Model

---
+++ ServerThread.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread
+{
+    protected Socket socket;
+    protected DataInputStream inputStream;
+    protected DataOutputStream outputStream;
+    private int ID;
+
+    public ServerThread(Socket clientSocket, DataInputStream inputStream, DataOutputStream outputStream, int ID) {
+        this.socket = clientSocket;
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+        this.ID = ID;
+    }
+    @Override
+    public void run() {
+        sendToClient(Integer.toString(this.ID));
+        while (true) {
+                listen();
+        }
+    }
+
+    public long getClientID(){
+        return this.ID;
+
+    }
+
+    public void listen(){
+        try {
+            //Distinguish what type of client it is
+
+            String receivedData = inputStream.readUTF();
+
+            if(!receivedData.isEmpty()){
+                if(receivedData.equals("#user")){
+                    System.out.println("Launch Client Handler");
+                }else if(receivedData.equals("#weather")){
+                    System.out.println("Launch Weather Handler");
+                }else if(receivedData.equals("!exit")){
+                    System.out.println("Closed Connection");
+                    closeConnection();
+
+                }
+
+            }
+
+        }catch(IOException e){
+            return;
+
+        }
+
+
+    }
+
+    public void closeConnection()
+    {
+            try {
+                this.socket.close();
+                this.inputStream.close();
+                this.outputStream.close();
+
+            }catch (IOException e){
+                e.printStackTrace();
+
+            }
+
+    }
+
+    public void sendToClient(String data)
+    {
+        try{
+
+            outputStream.writeUTF(data);
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
+
+
+
+
+    }
+
+
+
