fb9091d07ba64ecbfe6ee1ce9351e9af14532fe9
Imogen <imogenrwilkinson@gmail.com>
2019-04-21 21:28:57 +0100

    GUI and client changes

--- src/ClientUser.java
+++ src/ClientUser.java
@@ -16,10 +16,22 @@ public class ClientUser extends Client {
     private JPanel clientPanel;
     private JButton testBut;
     private JLabel idField;
+    private JTextField usernameInputField;
+    private JPasswordField passwordInputField;
+    private JButton createUserButton;
+    private JPanel loginPanel;
+    private JList stationDataList;
+    private JList connectedList;
+    private JButton disconnectBtn;
+    private JButton downloadBtn;
+    private JButton updateBtn;
+    private JButton loginOKButton;
+    private JButton loginCancelButton;
     private JList stationDisplayIDs;
     private String username;
     private String password;
     private boolean loggedIn = false;
+
     static private DefaultListModel stationListModel = new DefaultListModel();
     String[] serverIDs;
 
@@ -31,24 +43,35 @@ public class ClientUser extends Client {
         this.inputStream = inputStream;
         this.outputStream = outputStream;
         this.id = ID;
-        idField.setText(Integer.toString(id));
+        //idField.setText(Integer.toString(id));
     }
 
 
 
 
     public ClientUser() {
-        stationDisplayIDs.setModel(stationListModel);
+
         JFrame frame = new JFrame("User Client");
         frame.setContentPane(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(200, 200, 1000, 600);
         frame.setVisible(true);
+        connectedList.setModel(stationListModel);
 
-
-        testBut.addActionListener(new ActionListener() {
+        //sendToServer("!id");
+        updateBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 sendToServer("!id");
+            }
+        });
+
+
+        disconnectBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                closeConnection();
+                System.exit(0);
+
             }
         });
 
@@ -66,18 +89,39 @@ public class ClientUser extends Client {
                 //Stores the received data into "data"
                 String data = inputStream.readUTF();
                 System.out.println(data);
-                if (data.startsWith("!ids.")) {
+                if (data.startsWith("!ids."))
+                {
 
+                    ListModel oldIDs = connectedList.getModel();
                     serverIDs = data.substring(5).split(",");
-                    for(String s : serverIDs) {
-                        stationListModel.addElement(s);
-                        System.out.println(s);
+
+                    if (stationListModel.getSize()==0)
+                    {
+                        for (String s : serverIDs)
+                        {
+                            stationListModel.addElement(s);
+
+                        }
                     }
 
-                }
-            } catch (IOException e) {
+                    else
+                    {
+                        for (String s : serverIDs)
+                        {
+                            if (s != null)
+                            {
+                                if (!stationListModel.contains(s))
+                                {
+                                    stationListModel.addElement(s);
+                                }
 
+                            }
+
+                        }
+                    }
+                }
             }
+            catch (IOException e) { }
 
     }
 
